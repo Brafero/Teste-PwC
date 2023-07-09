dd6e260ea48fb7f8aa8f7c90836ab9aaa3b93214
Brafero <139059957+Brafero@users.noreply.github.com>
2023-07-09 12:18:10 -0300

    Update Exercicio 3 PwC.py

--- Exercicio 3 PwC.py
+++ Exercicio 3 PwC.py
@@ -1,12 +1,24 @@
 #Exercicio 3
+#Lógica usada: Para verificar os SubPalindromos de uma palavra, eles devem ser um subconjunto da palavra original e ser um Palindromo
+#Pensei em percorrer os elementos da palavra formando subconjuntos cada vez maiores e verificando se são palindromos
+#Caso fossem, também deveria verificar qual fosse o maior deles, então criei uma variável pra guardar somente o maior palindromo que encontrasse
+
 def subPalindromo(palavra):
-    maiorpal = ''                              #Cria uma variável para armazenar e comparar palindromos conforme são descobertos
-    for i in range(len(palavra)):              #Cria uma iteração da primeira à ultima letra da palavra
-        for j in range(i+1,len(palavra)+1):    #Cria outra iteração que parte da letra imediatamente depois daquela selecionada na iteração anterior até a ultima letra da palavra
-            temp = palavra[i:j]                #Cria uma variável temporária baseada nas iterações anteriores e assim percorrer todos os subconjuntos da palavra dada
-            if temp == temp[::-1]:             #Compara a palavra com ela mesma ao contrário
-                if len(temp)>len(maiorpal):    #Caso seja um palindromo maior que o já armazenado, armazena o maior
-                    maiorpal = temp          
+    #Cria uma variável para armazenar e comparar palindromos conforme são descobertos
+    maiorpal = ''       
+    
+    #Cria uma iteração para percorrer da primeira à ultima letra da palavra e outra iteração para comparar essa letra com todas as posteriores progressivamente
+    for i in range(len(palavra)):             
+        for j in range(i+1,len(palavra)+1):  
+            #Cria uma variável temporária para verificar se as letras contidas formam um palindromo
+            temp = palavra[i:j]                
+            if temp == temp[::-1]:   
+                #Caso seja um palindromo maior que o já armazenado, apaga o anterior e armazena o novo
+                if len(temp)>len(maiorpal):    
+                    maiorpal = temp       
+    #Devolve o palindromo armazenado
     print(maiorpal)
-#palavra = input()
-#subPalindromo(palavra)
+
+#Teste da função
+palavra = input()
+subPalindromo(palavra)
