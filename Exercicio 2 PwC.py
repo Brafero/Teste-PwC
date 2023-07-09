b751830bb5b20f077cff66b4ba6027f0f0ceba1d
Brafero <139059957+Brafero@users.noreply.github.com>
2023-07-09 12:23:30 -0300

    Update Exercicio 2 PwC.py

--- Exercicio 2 PwC.py
+++ Exercicio 2 PwC.py
@@ -1,11 +1,20 @@
 #Exercicio 2
+#Lógica usada: Para evitar que uma letra se repita, basta verificar se a letra já apareceu e não imprimi-la caso tenha aparecido
+
 def deletar_duplicado(palavra):
-  lista = []                             #Cria uma lista vazia para armazenar as letras conforme vão aparecendo
-  for i in range(len(palavra)):          #Começa uma iteração pra cada letra da palavra/frase
-    if palavra[i] in lista:              #Se a palavra já estiver na lista, será ignorada
+   #Cria uma lista vazia para armazenar as letras conforme vão aparecendo
+  lista = []         
+
+  #Começa uma iteração pra cada letra da palavra/frase
+  for i in range(len(palavra)):       
+    #Se a palavra já estiver na lista, será ignorada
+    if palavra[i] in lista:              
       continue
-    else:                                #Se não estiver na lista, sera impressa normalmente
+    #Se não estiver na lista, sera impressa normalmente
+    else:                               
       print(palavra[i], end='')
       lista.append(palavra[i])
-#palavra = input()
-#deletar_duplicado(palavra)
+
+#Teste da Função
+palavra = input()
+deletar_duplicado(palavra)
